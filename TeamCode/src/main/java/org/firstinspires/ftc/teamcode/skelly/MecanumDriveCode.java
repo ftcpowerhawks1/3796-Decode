b8f1eb6b260e270e6d6f4072605767ef5a639f43
Mint <123512601+mint-ium@users.noreply.github.com>
2025-10-07 18:26:32 -0400

    Mecanum Code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/skelly/MecanumDriveCode.java
@@ -0,0 +1,90 @@
+package org.firstinspires.ftc.teamcode.skelly;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+public class MecanumDriveCode {
+    private DcMotor frontLeftMotor, frontRightMotor, backLeftMotor, backRightMotor;
+    private IMU imu;
+
+    public void init(HardwareMap hwMap) {
+        //make sure configured correctly on hardware map
+        frontLeftMotor = hwMap.get(DcMotor.class, "frontLeftDrive");
+        frontRightMotor = hwMap.get(DcMotor.class, "frontRightDrive");
+        backLeftMotor = hwMap.get(DcMotor.class, "backLeftDrive");
+        backRightMotor = hwMap.get(DcMotor.class, "backRightDrive");
+        // We set the left motors in reverse which is needed for drive trains where the left
+        // motors are opposite to the right ones.
+        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        // This uses RUN_USING_ENCODER to be more accurate.   If you don't have the encoder
+        // wires, you should remove these
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        imu = hwMap.get(IMU.class, "imu");
+        // This needs to be changed to match the orientation on your robot
+        RevHubOrientationOnRobot RevOrientation = new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.RIGHT);
+
+        imu.initialize(new IMU.Parameters(RevOrientation));
+    }
+
+    public void drive(double forward, double strafe, double rotate) {
+        // This calculates the power needed for each wheel based on the amount of forward,
+        // strafe right, and rotate
+        double frontLeftPower = forward + strafe + rotate;
+        double frontRightPower = forward - strafe - rotate;
+        double backLeftPower = forward + strafe - rotate;
+        double backRightPower = forward - strafe + rotate;
+
+        double maxPower = 1.0;
+        double maxSpeed = 1.0; // make this slower for outreaches
+
+        // This is needed to make sure we don't pass > 1.0 to any wheel
+        // It allows us to keep all of the motors in proportion to what they should
+        // be and not get clipped
+        maxPower = Math.max(maxPower, Math.abs(frontLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(backLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(frontRightPower));
+        maxPower = Math.max(maxPower, Math.abs(backRightPower));
+
+        // We multiply by maxSpeed so that it can be set lower for outreaches
+        // When a young child is driving the robot, we may not want to allow full
+        // speed.
+        frontLeftMotor.setPower(maxSpeed * (frontLeftPower / maxPower));
+        frontRightMotor.setPower(maxSpeed * (frontRightPower / maxPower));
+        backLeftMotor.setPower(maxSpeed * (backLeftPower / maxPower));
+        backRightMotor.setPower(maxSpeed * (backRightPower / maxPower));
+    }
+
+    public void driveFieldRelative(double forward, double strafe, double rotate) {
+        // First, convert direction being asked to drive to polar coordinates
+        double theta = Math.atan2(forward, strafe);
+        double r = Math.hypot(strafe, forward);
+
+        // Second, rotate angle by the angle the robot is pointing
+        theta = AngleUnit.normalizeRadians(theta -
+                imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS));
+
+        // Third, convert back to cartesian
+        double newForward = r * Math.sin(theta);
+        double newStrafe = r * Math.cos(theta);
+
+        this.drive(newForward, newStrafe, rotate);
+    }
+}
