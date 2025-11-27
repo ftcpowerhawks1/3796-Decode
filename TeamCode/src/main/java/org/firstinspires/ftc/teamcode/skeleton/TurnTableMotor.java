557ec6c91dbcbd1d12d51a8b6c7d70c47c1cbe22
Mint <123512601+mint-ium@users.noreply.github.com>
2025-11-27 10:48:15 -0500

    CleaningUp
    Cleanup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/skeleton/TurnTableMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/skeleton/TurnTableMotor.java
@@ -13,6 +13,10 @@ public class TurnTableMotor {
     static int leftBound = -350; //Units:Ticks
     static int rightBound = 350; //Units:Ticks
     static double motorPower = 0.7;
+    static double TICKS_PER_REV = 2150.8; //19.2:1 Motor
+    static double DEGREES_PER_TICK = 360 / TICKS_PER_REV; //19.2:1 Motor
+
+    double tx = 0;
 
     public void init(HardwareMap hwMap) {
         motorTurn = hwMap.get(DcMotor.class, "motorTurn");
@@ -24,17 +28,18 @@ public class TurnTableMotor {
     public void track() {
         LLResult llResult = limelight.getLatestResult();
 
-        double tx = llResult.getTx();
+        tx = llResult.getTx();
 
-        double TICKS_PER_REV = 2150.8; //19.2:1 Motor
-        double DEGREES_PER_TICK = 360 / TICKS_PER_REV; //19.2:1 Motor
         int currentPos = motorTurn.getCurrentPosition();
 
         double txToTicks = (int) (tx / (DEGREES_PER_TICK)); //(degree)/(degree/ticks)
+
         motorTurn.setTargetPosition(currentPos - (int) txToTicks);
+
         if(llResult.isValid()) {
             motorTurn.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         }
+
         if (currentPos >= leftBound && currentPos <= rightBound) {
             if (txToTicks > 0) {
                 motorTurn.setPower(motorPower);
